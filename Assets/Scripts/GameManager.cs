ac0383e6c0941298dde6612ef9db01bd84150334
Spencer <43939691+spencer012@users.noreply.github.com>
2020-12-14 13:49:41 -0600

    Added placement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviourPunCallbacks {
 
 	public Transform spawns, checkpoints, players;
 	Dictionary<GameObject, int> ch;
+	Dictionary<int, Transform> hc;
 
 	public Text alert, position, lap;
 
@@ -41,14 +42,17 @@ public class GameManager : MonoBehaviourPunCallbacks {
 		Camera.main.GetComponent<DriftCamera>().SetTarget(w.lookAtTarget, w.positionTarget, w.sideView);
 
 		ch = new Dictionary<GameObject, int>();
+		hc = new Dictionary<int, Transform>();
 		for (int i = 0; i < checkpoints.childCount; i++) {
 			ch.Add(checkpoints.GetChild(i).gameObject, i);
+			hc.Add(i, checkpoints.GetChild(i));
 		}
 
 		PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { MultiProperties.LAP, 0 }, { MultiProperties.CHECKPOINT, 0 } });
 
-
 		lap.text = "Lap " + currentLap + "/" + numOfLaps;
+
+		cars = new Dictionary<Transform, Photon.Realtime.Player>();
 	}
 
 	public void CarEnter(GameObject c) {
@@ -70,10 +74,31 @@ public class GameManager : MonoBehaviourPunCallbacks {
 		}
 
 		lap.text = "Lap " + currentLap + "/" + numOfLaps;
+		position.text = "Pos " + 0 + "/" + PhotonNetwork.CurrentRoom.PlayerCount;
 	}
 	void Finish() {
 		playerCar.tag = "Player";
 		playerCar.transform.GetChild(0).gameObject.tag = "Player";
+
+		alert.gameObject.SetActive(true);
+		alert.text = "Finished!";
+
+		PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { MultiProperties.FINISHED, true } });
+
+		bool allFinished = true;
+
+		foreach (Photon.Realtime.Player p in PhotonNetwork.PlayerListOthers) {
+			if (!p.CustomProperties.ContainsKey(MultiProperties.FINISHED)) {
+				allFinished = false;
+			}
+		}
+
+		if (allFinished) {
+			Invoke("Return", 1);
+		}
+	}
+	void Return() {
+		PhotonNetwork.LoadLevel(1);
 	}
 
 
@@ -81,6 +106,49 @@ public class GameManager : MonoBehaviourPunCallbacks {
 		if (Input.GetKeyDown(KeyCode.R) && !resetCooldown) {
 			ResetCar();
 		}
+	}
+
+	Dictionary<Transform, Photon.Realtime.Player> cars;
+	public void RegisterCar(Transform g) {
+		cars.Add(g, g.gameObject.GetComponent<PhotonView>().Owner);
+	}
+	public void UnRegisterCar(Transform g) {
+		cars.Remove(g);
+	}
+
+	void FixedUpdate() {
+		if (playerCar != null) {
+			bool succeed = true;
+			object check, lp;
+			succeed &= PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(MultiProperties.CHECKPOINT, out check);
+			succeed &= PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(MultiProperties.LAP, out lp);
+			if (!succeed)
+				return;
+
+			float myPos = ((int)check + 1) * ((int)lp + 1) * 1000000 - Vector3.Distance(playerCar.transform.position, hc[mod((int)check + 1, ch.Count)].position);
+			int pos = 0;
+			print(myPos + " my");
+
+			foreach (Transform t in players) {
+				if (t == playerCar.transform)
+					continue;
+
+				succeed &= cars[t].CustomProperties.TryGetValue(MultiProperties.CHECKPOINT, out check);
+				succeed &= cars[t].CustomProperties.TryGetValue(MultiProperties.LAP, out lp);
+				if (!succeed)
+					return;
+
+				if (((int)check + 1) * ((int)lp + 1) * 1000000 - Vector3.Distance(t.position, hc[mod((int)check + 1, ch.Count)].position) > myPos) {
+					pos++;
+				}
+				print(((int)check + 1) * ((int)lp + 1) * 1000000 - Vector3.Distance(t.position, hc[mod((int)check + 1, ch.Count)].position));
+			}
+
+			position.text = "Pos " + (pos + 1) + "/" + PhotonNetwork.CurrentRoom.PlayerCount;
+		}
+	}
+	int mod(int x, int m) {
+		return (x % m + m) % m;
 	}
 
 	bool resetCooldown = false;
